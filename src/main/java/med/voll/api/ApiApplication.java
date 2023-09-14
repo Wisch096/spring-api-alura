6bc27118d2ad907474df3b2a05d002f2a690b01a
Wisch096 <wisch.pg@outlook.com>
2023-09-14 20:32:53 -0300

    task

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package med.voll.api;
 
+import med.voll.api.medico.Medico;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
 }
